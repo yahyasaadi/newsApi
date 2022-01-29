72871e53076d012ff0576dee5f2230f58474e6aa
yahyasaadi <yahyasaadi9219@gmail.com>
2022-01-29 07:46:23 +0300

    make a spelling correction

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    title = "New API App"
+    title = "News API App"
     para = "It is working for now"
     return render_template('index.html', temp_title=title, temp_para = para)
 
