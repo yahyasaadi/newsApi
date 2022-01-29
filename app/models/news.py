6bf03b649c2599177bdc159c9e10d7f3c63faef4
yahyasaadi <yahyasaadi9219@gmail.com>
2022-01-29 07:25:08 +0300

    added a news file to hold the news class

---
+++ app/models/news.py
@@ -0,0 +1,10 @@
+
+class News:
+    
+    def __init__(self, id, name, url, category, country):
+        
+        self.id = id
+        self.name = name
+        self.url = url
+        self.category = category
+        self.country = country
