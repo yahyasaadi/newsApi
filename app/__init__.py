159a38146d8d9c5767342380f675e075895d8fbf
yahyasaadi <yahyasaadi9219@gmail.com>
2022-01-31 09:16:47 +0300

    imported the flask-bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
 from .config import DevConfig
+from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__, instance_relative_config = True)
+
+
+# Initializing bootstap
+bootstrap = Bootstrap(app)
 
 # Setting up config settinga
 app.config.from_object(DevConfig)
